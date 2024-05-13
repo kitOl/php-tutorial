cc715eafa0a4f7f8e85b62192c3f9e7cecc0251d
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-13 20:49:35 +0300

    23 add basic files

---
+++ 23/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Tutorial</title>
+  <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+
+  <header>
+    <nav>
+      <ul>
+        <li><a href="">nav1</a></li>
+        <li><a href="">nav2</a></li>
+        <li><a href="">nav3</a></li>
+        <li><a href="">nav4</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <section>
+    <div class="someMainContent">
+      <h1>Hi there!</h1>
+    </div>
+  </section>
+</body>
+
+</html>
