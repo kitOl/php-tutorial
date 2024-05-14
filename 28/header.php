7fb5254a53b67a4ae153867eadbbe17fb35c68c7
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-14 04:12:20 +0300

    28 add styles

--- 28/header.php
+++ 28/header.php
@@ -9,6 +9,7 @@ session_start();
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PHP Tutorial::SESSION</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
