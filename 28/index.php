511bb2f75164c89c70e5562fb002494cb0f4c9a6
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-14 03:57:21 +0300

    add basic files

---
+++ 28/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Tutorial::SESSION</title>
+</head>
+
+<body>
+  <ul>
+    <li><a href="index.php">HOME</a></li>
+    <li><a href="contact.php">CONTACT</a></li>
+  </ul>
+
+  <?php
+  $_SESSION['username'] = 'Oleg';
+  ?>
+</body>
+
+</html>
