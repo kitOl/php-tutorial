62ac2f298563fd55ffefa7f23757d4e1cf0bf873
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-14 19:46:12 +0300

    42 add new error handler and styles

---
+++ 42/includes/signup.inc.php
@@ -0,0 +1,33 @@
+<?php
+
+if (isset($_POST['submit'])) {
+  include_once 'dbh.inc.php';
+
+  $first = $_POST['first'];
+  $last = $_POST['last'];
+  $email = $_POST['email'];
+  $uid = $_POST['uid'];
+  $pwd = $_POST['pwd'];
+
+  if (empty($first) || empty($last) || empty($email) || empty($uid) || empty($pwd)) {
+    header("Location: ../index.php?signup=empty");
+    exit();
+  } else {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      header("Location: ../index.php?signup=invalidemail");
+      exit();
+    } else if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$/", $last)) {
+      header("Location: ../index.php?signup=char");
+      exit();
+    } else if (!preg_match("/^[a-zA-Z0-9]*$/", $uid)) {
+      header("Location: ../index.php?signup=username");
+      exit();
+    } else {
+      header("Location: ../index.php?signup=success");
+      exit();
+    }
+  }
+} else {
+  header("Location: ../index.php?signup=error");
+  exit();
+}
---
+++ 42/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Sign Up</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <h2>Sign Up</h2>
+  <form action="includes/signup.inc.php" method="post">
+    <input type="text" name="first" placeholder="Firstname">
+    <br>
+    <input type="text" name="last" placeholder="Lastname">
+    <br>
+    <input type="text" name="email" placeholder="E-mail">
+    <br>
+    <input type="text" name="uid" placeholder="Username">
+    <br>
+    <input type="password" name="pwd" placeholder="Password">
+    <br>
+
+    <button type="submit" name="submit">Sing up</button>
+  </form>
+
+  <p><?= $_GET['signup'] ?? '' ?>
+
+</body>
+
+</html>
