985a66420454ba454fa8663f3194afdb12f1021b
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-15 08:19:16 +0300

    51 add basic files for upload files

---
+++ 51/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Tutorial::Upload Files</title>
+</head>
+
+<body>
+
+  <form action="upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="file" placeholder="Choice file">
+    <button type="submit" name="submit">UPLOAD</button>
+  </form>
+
+</body>
+
+</html>
