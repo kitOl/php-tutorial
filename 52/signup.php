31af52292c7fc78d9d94496a7aabc0b1ab223b7e
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-16 02:53:58 +0300

    add signup page

---
+++ 52/signup.php
@@ -0,0 +1,27 @@
+<?php
+
+$first = $_POST['first'];
+$last = $_POST['last'];
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+
+$sql = "INSERT INTO users (first, last, username, password)
+VALUES ('$first', '$last', '$uid', '$pwd');";
+mysqli_query($conn, $sql);
+
+$sql = "SELECT * FROM users WHERE username = '$uid' AND first = '$first';";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result)) {
+  while ($row = mysqli_fetch_assoc($result)) {
+    $userId = $row['id'];
+
+    $sql = "INSERT INTO profileimg (userid, status)
+    VALUES ('$userId', 1);";
+    mysqli_query($conn, $sql);
+    header("Location: index.php");
+    exit();
+  }
+} else {
+  echo "You have an error!";
+}
