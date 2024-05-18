fcec87eae9afdff0eb3d637ad4af36b360711983
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-18 14:28:42 +0300

    56 add deleting file by name any extention

--- 56/deletefile.php
+++ 56/deletefile.php
@@ -1,7 +1,10 @@
 <?php
-$path = 'uploads/profile1.jpeg';
 
-if (!unlink($path)) {
+$path = 'uploads/profile1*';
+$fileInfo = glob($path);
+$fileActualName = $fileInfo[0];
+
+if (!unlink($fileActualName)) {
   echo "You have an error!";
   header("Location: index.php?deletefile=error");
   exit();
