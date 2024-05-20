8c411fce8f1c6c76ad33062af931f2aefb736a03
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-20 06:06:58 +0300

    65 add basic files from 64

---
+++ 65/gallery.php
@@ -0,0 +1,100 @@
+<?php
+session_start();
+$_SESSION['username'] = 'admin';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>My Portfolio</title>
+  <link href="https://fonts.googleapis.com/css?family=Catamaran:100,200,300,regular,500,600,700,800,900" rel="stylesheet" />
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+  <header>
+    <a href="index.php" class="header-brand">oltuts</a>
+    <nav>
+      <ul>
+        <li><a href="portfolio.php">Portfolio</a></li>
+        <li><a href="about.php">About me</a></li>
+        <li><a href="contact.php">Contact</a></li>
+      </ul>
+      <a href="cases.php" class="header-cases">Cases</a>
+    </nav>
+  </header>
+  <main>
+    <section class="gallery-links">
+      <div class="wrapper">
+        <h2>Gallery</h2>
+
+        <div class="gallery-container">
+          <a href="#">
+            <div></div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+          </a>
+          <a href="#">
+            <div></div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+          </a>
+          <a href="#">
+            <div></div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+          </a>
+          <a href="#">
+            <div></div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+          </a>
+          <a href="#">
+            <div></div>
+            <h3>This is a title</h3>
+            <p>This is a paragraph</p>
+          </a>
+        </div>
+
+        <?php if (isset($_SESSION['username'])) {
+        } ?>
+        <div class="gallery-upload">
+          <form action="includes/gallery-upload.inc.php" method="post">
+            <input type="text" name="filename" placeholder="File name...">
+            <input type="text" name="filetitle" placeholder="Image title...">
+            <input type="text" name="filedesc" placeholder="Image description...">
+            <input type="file" name="file">
+            <button type="submit" name="submit">UPLOAD</button>
+          </form>
+        </div>
+      </div>
+    </section>
+  </main>
+
+  <footer>
+    <div class="wrapper">
+      <ul class="footer-links-main">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Cases</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">About me</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <ul class="footer-links-cases">
+        <li>
+          <p>Latest cases</p>
+        </li>
+        <li><a href="#">MALING SHAOLIN - WEB DEVELOPMENT</a></li>
+        <li><a href="#">EXCELLENTO - WEB DEVELOPMENT</a></li>
+        <li><a href="#">OLEGTUTS - YOUTUBE CHANNEL</a></li>
+        <li><a href="#">WELTEC - VIDEO PRODUCTDION</a></li>
+      </ul>
+
+    </div>
+  </footer>
+</body>
+
+</html>
