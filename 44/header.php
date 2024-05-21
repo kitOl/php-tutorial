5c74505186f3271bfebf030728f580969c796a14
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-05-21 07:02:24 +0300

    44 add basic files and styles for Login system

---
+++ 44/header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP Tutorial:: Login System</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <header>
+    <nav class="nav-header-main">
+      <a class="header-logo" href="index.php">
+        <img src="img/logo.png" alt="logo">
+      </a>
+      <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="#">Portfolio</a></li>
+        <li><a href="#">About me</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <div class="header-login">
+        <form action="includes/login.inc.php" method="post">
+          <input type="text" name="mailuid" placeholder="Username/E-mail...">
+          <input type="password" name="pwd" placeholder="Password">
+          <button type="submit" name="login-submit">Login</button>
+        </form>
+        <a href="signup.php">Signup</a>
+        <form action="includes/logout.inc.php" method="post">
+          <button type="submit" name="logout-submit">Logout</button>
+        </form>
+      </div>
+    </nav>
+
+  </header>
